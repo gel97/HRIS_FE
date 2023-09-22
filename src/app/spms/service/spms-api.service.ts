f2706e5c6d0f4660311b2fbfe5aad0d77789d1b6
gel97 <alangilan.rogel@gmail.com>
2023-09-22 11:05:07 +0800

    update service

---
+++ src/app/spms/service/spms-api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { api } from 'src/app/connection';
+@Injectable({
+  providedIn: 'root'
+})
+export class SpmsApiService {
+
+  constructor() { }
+  get_mfoes = (officeId:string)=> `/tPMMFOes/${officeId}`;
+  post_mfo_search_office = ()=> `/tPMMFOes/Search`;
+
+}
