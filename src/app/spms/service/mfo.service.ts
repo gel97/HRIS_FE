8d071fec7d49b4d9d1eb09f291848cf827d24c65
largolowell <davelargo01@gmail.com>
2023-09-22 11:58:48 +0800

    backup

--- src/app/spms/service/mfo.service.ts
+++ src/app/spms/service/mfo.service.ts
@@ -1,48 +1,53 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { SpmsApiService } from "../../../service/api/spms-api.service";
-import { api } from "../../../service/api/connection";
+import { SpmsApiService } from './spms-api.service';
+import { api } from 'src/app/connection';
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class MfoService {
-  mfo:Observable<any[]> = of( [
+  mfo: Observable<any[]> = of([
     {
-      MFOId: "MFO1234",
-      MFO: "Sample MFO 1",
+      MFOId: 'MFO1234',
+      MFO: 'Sample MFO 1',
       category: 1,
-      si: [{ indicatorId: "IND1234", indicator: "Sample IND 1", target: 10 },{ indicatorId: "IND121314", indicator: "Sample IND 4", target: 50} ],
+      si: [
+        { indicatorId: 'IND1234', indicator: 'Sample IND 1', target: 10 },
+        { indicatorId: 'IND121314', indicator: 'Sample IND 4', target: 50 },
+      ],
     },
     {
-      MFOId: "MFO5678",
-      MFO: "Sample MFO 2",
+      MFOId: 'MFO5678',
+      MFO: 'Sample MFO 2',
       category: 2,
-      si: [{ indicatorId: "IND5678", indicator: "Sample IND 2", target: 20 }],
+      si: [{ indicatorId: 'IND5678', indicator: 'Sample IND 2', target: 20 }],
     },
     {
-      MFOId: "MFO91011",
-      MFO: "Sample MFO 3",
+      MFOId: 'MFO91011',
+      MFO: 'Sample MFO 3',
       category: 3,
-      si: [{ indicatorId: "IND91011", indicator: "Sample IND 3", target: 30 }],
-    }
+      si: [{ indicatorId: 'IND91011', indicator: 'Sample IND 3', target: 30 }],
+    },
   ]);
 
   constructor(private http: HttpClient, private url: SpmsApiService) {}
 
   GetMFOes(officeId: string) {
     return this.http.get<any[]>(api + this.url.get_mfoes(officeId), {
-      responseType: "json",
+      responseType: 'json',
     });
   }
 
   SearchMfoOffice(payload: any) {
-    return this.http.post<any[]>(api + this.url.post_mfo_search_office(), payload , {
-      responseType: "json",
-    });
+    return this.http.post<any[]>(
+      api + this.url.post_mfo_search_office(),
+      payload,
+      {
+        responseType: 'json',
+      }
+    );
   }
 
   AddMFO() {}
-
- 
 }
--- src/app/spms/service/spms-api.service.ts
+++ src/app/spms/service/spms-api.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { api } from 'src/app/connection';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpmsApiService {
+  constructor() {}
+  get_mfoes = (officeId: string) => `/tPMMFOes/${officeId}`;
+  post_mfo_search_office = () => `/tPMMFOes/Search`;
 
-  constructor() { }
-  get_mfoes = (officeId:string)=> `/tPMMFOes/${officeId}`;
-  post_mfo_search_office = ()=> `/tPMMFOes/Search`;
-
+  get_opcrs = (year: string) => `/t`;
 }
