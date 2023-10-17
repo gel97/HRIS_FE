a0f3328bf2f4523db0078da996ce591e5c12454e
largolowell <davelargo01@gmail.com>
2023-10-17 08:17:19 +0800

    update

--- src/app/spms/pages/dpcr/dpcr-actual/dpcr-actual.component.ts
+++ src/app/spms/pages/dpcr/dpcr-actual/dpcr-actual.component.ts
@@ -6,12 +6,47 @@ import { DpcrService } from 'src/app/spms/service/dpcr.service';
   templateUrl: './dpcr-actual.component.html',
   styleUrls: ['./dpcr-actual.component.css'],
 })
-export class DpcrActualComponent {
+export class DpcrActualComponent implements OnInit {
   dpcrService = inject(DpcrService);
   dpcrMfoes: any = this.dpcrService.dpcrData();
 
   ngOnInit(): void {
     this.dpcrService.GetDpcrData();
     console.log('dpcrmfoeeeess', this.dpcrMfoes);
+  }
+
+  get dpcrMfoesData(): { [key: number]: any[] } {
+    const groupedData: { [key: number]: any[] } = this.dpcrMfoes.data.reduce(
+      (acc: any, data: any) => {
+        const { categoryId } = data;
+        if (acc[categoryId]) {
+          acc[categoryId].push(data);
+        } else {
+          acc[categoryId] = [data];
+        }
+        return acc;
+      },
+      {}
+    );
+    return groupedData;
+  }
+
+  categoryName(cat: any) {
+    let catName = '';
+    switch (cat) {
+      case '1':
+        catName = 'STRATEGIC';
+        break;
+      case '2':
+        catName = 'CORE';
+        break;
+      case '3':
+        catName = 'SUPPORT';
+        break;
+      default:
+        break;
+    }
+
+    return catName ? catName + ' FUNCTION' : 'NO FUNCTION';
   }
 }
