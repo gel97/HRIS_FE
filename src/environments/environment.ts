8dbfb9d2d159d1021eca609b41e359ab036df9ca
gel97 <alangilan.rogel@gmail.com>
2024-10-18 10:44:30 +0800

    fix view lab report, ticket add multiple images, and create environtment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     apiUrl: 'https://localhost:7114/api',
 
-    hwApiUrl: 'https://davnorsystems.gov.ph/HWWebApi/api',
-    hwApiKey: '3d5e37ce758bcc60f4eac855db41c6e46b3faac700ba59f37d895112eed41a3a3c41876152c186563129b47280314330e440680e2dd7a72fd5be4f50bd4b2c4b',
+    hwApiUrl: '',
+    hwApiKey: '',
 
   };
